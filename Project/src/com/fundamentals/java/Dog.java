3a0a47e21ee54e4488173487ec718ea46c0bf1e3
Cynthia Alaniz <Cynthia.alaniz1995@gmail.com>
2018-10-22 15:09:08 -0500

    Today we did Array examples

---
+++ Project/src/com/fundamentals/java/Dog.java
@@ -0,0 +1,19 @@
+package com.fundamentals.java;
+
+public class Dog {
+	
+	public String paws; 
+	
+	public String nose; 
+	
+	public String tail; 
+	
+	public void openDoor() {
+		
+	}
+	
+	public void closeDoor() {
+		
+	}
+
+}
